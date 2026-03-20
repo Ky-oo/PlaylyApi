1708fcdb2937b4068c2ceb3e908f910454e17b2d
Ky-oo <kylian.patry@gmail.com>
2026-03-20 11:49:21 +0100

    Configure Sequelize instance with MySQL dialect and disable logging

--- orm.js
+++ orm.js
@@ -1,6 +1,9 @@
 const { Sequelize } = require("sequelize");
 
 let sequelizeInstance;
-sequelizeInstance = new Sequelize(process.env.DB_URI);
+sequelizeInstance = new Sequelize(process.env.DB_URI, {
+  dialect: "mysql2",
+  logging: false,
+});
 
 module.exports = sequelizeInstance;
